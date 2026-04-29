3f32121b9335c74099e7e5ce8244c3e0fb958586
Karthikagayu <106004276+Karthikagayu@users.noreply.github.com>
2026-04-29 12:49:35 +0530

    latest push

--- src/backend/server.js
+++ src/backend/server.js
@@ -43,6 +43,6 @@ app.delete("/customers/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.PORT(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
